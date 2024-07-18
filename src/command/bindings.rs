30c0edca6ddba6c0cff8d16df856f366e0448d2e
Szabolcs Seláf <selu@selu.org>
2024-07-18 12:31:41 +0200

    remove formatting changes

--- src/command/bindings.rs
+++ src/command/bindings.rs
@@ -15,7 +15,7 @@ extern "C" {
     /// [Events](https://tauri.app/v1/guides/features/events)
     #[cfg(feature = "event")]
     #[doc(cfg(feature = "event"))]
-    // for some reason this doc comment is seen as unused...
+    // for some reason this doc comment is seen as unused... 
     #[allow(unused_doc_comments)]
     #[wasm_bindgen(catch, js_namespace = ["window", "__TAURI__", "event"])]
     pub async fn listen(
@@ -42,13 +42,13 @@ async fn wrapped_invoke(command: &str, args: JsValue) -> InvokeResult {
                     return InvokeResult::NotRegistered;
                 }
             }
-
+            
             InvokeResult::Err(value)
-        }
+        },
     }
 }
 
-/// Wrapper for [wait_invoke], to send a command without waiting for it
+/// Wrapper for [wait_invoke], to send a command without waiting for it 
 pub fn fire_and_forget_invoke(command: &'static str, args: JsValue) {
     wasm_bindgen_futures::spawn_local(wait_invoke(command, args))
 }
@@ -79,12 +79,10 @@ where
     E: DeserializeOwned,
 {
     match wrapped_invoke(command, args).await {
-        InvokeResult::Ok(value) => {
-            Ok(serde_wasm_bindgen::from_value(value).unwrap_or_else(|why| {
-                log::error!("Conversion failed: {why}");
-                Default::default()
-            }))
-        }
+        InvokeResult::Ok(value) => Ok(serde_wasm_bindgen::from_value(value).unwrap_or_else(|why| {
+            log::error!("Conversion failed: {why}");
+            Default::default()
+        })),
         InvokeResult::Err(value) => Err(serde_wasm_bindgen::from_value(value).unwrap()),
         InvokeResult::NotRegistered => Ok(Default::default()),
     }
